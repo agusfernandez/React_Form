392123670eb691a3c8f0e9d2fc33e5188b471bf1
agusfernandez <agustina.fer91@gmail.com>
2021-06-24 00:59:56 -0300

    test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import UsersList from "./components/UsersList/";
 import Header from "./components/Header/"
 import {useState} from "react";
 
-// los componentes se pasan de padres a hijos por ello el add user ak tener todo los componentes es el que se va engargar de agregar lso datos. le vana  llegar los usuaris que entran por el user form para pasarlo a la lista
+// los componentes se pasan de padres a hijos por ello el add user ak tener todo los componentes es el que se va engargar de agregar lso datos. le vana  llegar los usuaris que entran por el user form para pasarlo a la lista ///pppp
 const App =()=> {
   const [users, setUsers] = useState([]);
   // console.log( users);
